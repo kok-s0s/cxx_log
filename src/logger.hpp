2870189ef9802d1b51256164c1fbf7cc4f424731
kok-s0s <2694308562@qq.com>
2023-12-21 12:09:14 +0800

    log - simple / warning / error

--- src/logger.hpp
+++ src/logger.hpp
@@ -1,31 +1,48 @@
+#include <chrono>
 #include <ctime>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <string>
+
+namespace klog {
 
 class Logger {
 public:
+  enum LogLevel { INFO, WARNING, ERROR };
+
   static Logger &getInstance(const std::string &filename) {
     static Logger instance(filename);
     return instance;
   }
 
   template <typename T> Logger &operator<<(const T &value) {
-    if (log_file_.is_open()) {
-      log_file_ << value << std::endl;
-    } else {
-      std::cerr << "Log file is not open." << std::endl;
-    }
+    log(value);
     return *this;
   }
 
-  void log(const std::string &message) {
+  void log(const std::string &message, LogLevel level = INFO) {
     if (log_file_.is_open()) {
-      std::time_t now = std::time(0);
-      std::tm *timeInfo = std::localtime(&now);
+      auto now = std::chrono::system_clock::now();
+      std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+      auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    now.time_since_epoch()) %
+                1000;
 
-      log_file_ << "[" << std::put_time(timeInfo, "%Y-%m-%d %X") << "] "
-                << message << std::endl;
+      std::tm *timeInfo = std::localtime(&now_c);
+
+      // ANSI escape codes for text color
+      const char *colorCode = getColorCode(level);
+
+      // Output log to file
+      log_file_ << "[" << std::put_time(timeInfo, "%Y-%m-%d %X") << "."
+                << std::setw(3) << std::setfill('0') << ms.count() << "] "
+                << getLogLevelString(level) << ": " << message << std::endl;
+
+      // Output colored log to console
+      std::cout << "[" << std::put_time(timeInfo, "%Y-%m-%d %X") << "."
+                << std::setw(3) << std::setfill('0') << ms.count() << "] "
+                << colorCode << getLogLevelString(level) << ": " << message
+                << "\033[0m" << std::endl;
     } else {
       std::cerr << "Log file is not open." << std::endl;
     }
@@ -46,4 +63,32 @@ private:
   }
 
   std::ofstream log_file_;
+
+  const char *getColorCode(LogLevel level) const {
+    switch (level) {
+    case INFO:
+      return "\033[1;32m"; // Green
+    case WARNING:
+      return "\033[1;33m"; // Yellow
+    case ERROR:
+      return "\033[1;31m"; // Red
+    default:
+      return "\033[0m"; // Default color (reset)
+    }
+  }
+
+  const char *getLogLevelString(LogLevel level) const {
+    switch (level) {
+    case INFO:
+      return "INFO";
+    case WARNING:
+      return "WARNING";
+    case ERROR:
+      return "ERROR";
+    default:
+      return "";
+    }
+  }
 };
+
+} // namespace klog
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "logger.hpp"
 
 int main() {
-  Logger &logger = Logger::getInstance("example.log");
-  logger.log("This is a sample log message.");
+  klog::Logger &logger = klog::Logger::getInstance("example.log");
+  logger << "This is a sample log message.";
+  logger.log("This is an informational message.", klog::Logger::INFO);
+  logger.log("This is a warning message.", klog::Logger::WARNING);
+  logger.log("This is an error message.", klog::Logger::ERROR);
 
   return 0;
 }
